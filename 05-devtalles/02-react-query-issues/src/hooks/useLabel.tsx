c94522bf4549404837bf6354bf9eb9db220cedc4
Fernando Villalba <21fervillalbag@gmail.com>
2023-09-02 03:04:44 -0400

    feat: use query parameters advanced

--- 05-devtalles/02-react-query-issues/src/hooks/useLabel.tsx
+++ 05-devtalles/02-react-query-issues/src/hooks/useLabel.tsx
@@ -9,7 +9,54 @@ const getLabels = async (): Promise<Label[]> => {
 };
 
 export const useLabels = () => {
-  const labelQuery = useQuery(["labels"], getLabels);
+  const labelQuery = useQuery(["labels"], getLabels, {
+    // Esto nos permitira no realizar una consulta
+    // nuevamente hasta dentro de una hora
+    // staleTime: 1000 * 60 * 60,
+
+    // Esta data se mostrar mientras se cumpla el staleTime
+    // es decir si tenemos nuestro stale time durante 1h
+    // durante 1h se mostrara este initialData
+    // initialData: [
+    //   {
+    //     id: 196858374,
+    //     node_id: "MDU6TGFiZWwxOTY4NTgzNzQ=",
+    //     url: "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+    //     name: "CLA Signed",
+    //     color: "e7e7e7",
+    //     default: false,
+    //   },
+    //   {
+    //     id: 1649755876,
+    //     node_id: "MDU6TGFiZWwxNjQ5NzU1ODc2",
+    //     url: "https://api.github.com/repos/facebook/react/labels/Component:%20Fast%20Refresh",
+    //     name: "Component: Fast Refresh",
+    //     color: "473bcc",
+    //     default: false,
+    //   },
+    // ],
+
+    // Estos datos se pueden mostrar antes de que
+    // se termine de hacer la carga de datos desde la API
+    placeholderData: [
+      {
+        id: 196858374,
+        node_id: "MDU6TGFiZWwxOTY4NTgzNzQ=",
+        url: "https://api.github.com/repos/facebook/react/labels/CLA%20Signed",
+        name: "CLA Signed",
+        color: "e7e7e7",
+        default: false,
+      },
+      {
+        id: 1649755876,
+        node_id: "MDU6TGFiZWwxNjQ5NzU1ODc2",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Fast%20Refresh",
+        name: "Component: Fast Refresh",
+        color: "473bcc",
+        default: false,
+      },
+    ],
+  });
 
   return labelQuery;
 };
--- 05-devtalles/02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 05-devtalles/02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,11 +1,19 @@
 import { useLabels } from "../../hooks/useLabel";
+import Loading from "../../share/components/Loading";
 
-export const LabelPicker = () => {
+interface LabelPickerProps {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker = ({
+  onChange,
+  selectedLabels,
+}: LabelPickerProps) => {
   const labelsQuery = useLabels();
-  console.log(labelsQuery);
 
   if (labelsQuery.isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   return (
@@ -13,11 +21,14 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
